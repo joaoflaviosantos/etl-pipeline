cedb84571df294cf6f9a410c15a0d9b3fd0b684d
João Flávio Santos <joaoflavio.s@hotmail.com>
2022-04-24 03:09:53 -0300

    FEAT: Implementing auto pre-commit coding tester on extract_html_test

--- src/tests/tester.sh
+++ src/tests/tester.sh
@@ -1,1 +1,1 @@
-pytest -s -v src/drivers/html_collector_test.py src/drivers/http_requester_test.py
+pytest -s -v src/drivers/html_collector_test.py src/drivers/http_requester_test.py src/stages/extract/extract_html_test.py
