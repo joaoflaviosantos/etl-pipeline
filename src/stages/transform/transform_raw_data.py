74ee11f0eed566db43729b93b387d1b28a5aea88
João Flávio Santos <joaoflavio.s@hotmail.com>
2022-04-24 06:56:41 -0300

    Feat: Finishing implementing transform stage

--- src/stages/transform/transform_raw_data.py
+++ src/stages/transform/transform_raw_data.py
@@ -1,12 +1,20 @@
 # pylint: disable=multiple-statements
 from typing import List, Dict
 from src.stages.contracts.extract_contract import ExtractContract
+from src.stages.contracts.transform_contract import TransformContract
+from src.errors.transform_error import TransformError
 
 class TransformRawData:
 
-    def transform(self, extract_contract: ExtractContract):
-        transformed_information = self.__filter_and_transform_data(extract_contract)
-        return transformed_information
+    def transform(self, extract_contract: ExtractContract) -> TransformContract:
+        try:
+            transformed_information = self.__filter_and_transform_data(extract_contract)
+            transform_data_contract = TransformContract(
+                load_content=transformed_information
+            )
+            return transform_data_contract
+        except Exception as exception:
+            raise TransformError(str(exception)) from exception
 
     def __filter_and_transform_data(self, extract_contract: ExtractContract) -> List[Dict]:
         extraction_date = extract_contract.extraction_date
@@ -25,7 +33,7 @@ class TransformRawData:
 
             transformed_data = self.__transform_data(names, link)
             transformed_data["extraction_date"] = extraction_date
-            print(transformed_data)
+            #print(transformed_data)
             transformed_information.append(transformed_data)
 
         return transformed_information
--- src/stages/transform/transform_raw_data_test.py
+++ src/stages/transform/transform_raw_data_test.py
@@ -1,8 +1,24 @@
 from src.stages.contracts.mocks.extract_contract import extract_contract_mock
+from src.stages.contracts.transform_contract import TransformContract
+from src.errors.transform_error import TransformError
 from .transform_raw_data import TransformRawData
 
 def test_transform():
     transform_raw_data = TransformRawData()
-    transform_raw_data.transform(extract_contract_mock)
-    #print()
-    #print(transformed_information)
+    transform_data_contract = transform_raw_data.transform(extract_contract_mock)
+
+    assert isinstance(transform_data_contract, TransformContract)
+    assert "first_name" in transform_data_contract.load_content[0]
+    assert "second_name" in transform_data_contract.load_content[0]
+    assert "surname" in transform_data_contract.load_content[0]
+    assert "artist_id" in transform_data_contract.load_content[0]
+    assert "link" in transform_data_contract.load_content[0]
+    assert "extraction_date" in transform_data_contract.load_content[0]
+
+def test_transform_error():
+    transform_raw_data = TransformRawData()
+    try:
+        transform_raw_data.transform('Entry with error')
+    except Exception as exception: # pylint: disable=broad-except
+        print(exception)
+        assert isinstance(exception, TransformError)
