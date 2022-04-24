865d292c2eb00830f2d5ebcc483ba13a740c0100
João Flávio Santos <joaoflavio.s@hotmail.com>
2022-04-24 01:03:55 -0300

    feat: Implementing extract_contract and extract_error with tests

--- src/stages/extract/extract_html.py
+++ src/stages/extract/extract_html.py
@@ -1,6 +1,9 @@
+from datetime import date
 from typing import Type
 from src.drivers.interfaces.http_requester import HttpRequesterInterface
 from src.drivers.interfaces.html_collector import HtmlCollectorInterface
+from src.stages.contracts.extract_contract import ExtractContract
+from src.errors.extract_error import ExtractError
 
 class ExtractHtml:
 
@@ -8,7 +11,13 @@ class ExtractHtml:
         self.__http_requester = http_requester
         self.__html_collector = html_collector
 
-    def extact(self):
-        html_information = self.__http_requester.request_from_page()
-        essential_information = self.__html_collector.collect_essential_information(html_information["html"])
-        return essential_information
+    def extract(self) -> ExtractContract:
+        try:
+            html_information = self.__http_requester.request_from_page()
+            essential_information = self.__html_collector.collect_essential_information(html_information["html"])
+            return ExtractContract(
+                raw_information_content=essential_information,
+                extraction_date=date.today()
+            )
+        except Exception as exception:
+            raise ExtractError(str(exception)) from exception
--- src/stages/extract/extract_html_test.py
+++ src/stages/extract/extract_html_test.py
@@ -1,15 +1,32 @@
 from src.drivers.http_requester import HttpRequester
 from src.drivers.html_collector import HtmlCollector
+from src.stages.contracts.extract_contract import ExtractContract
+from src.errors.extract_error import ExtractError
 
 from .extract_html import ExtractHtml
 
-def test_extract_html():
+def test_extract():
     # Importing tested class
     http_requester = HttpRequester()
     html_extractor = HtmlCollector()
 
     # Object testing intancing (Dependency Injection)
     extract_html = ExtractHtml(http_requester,html_extractor)
-    response = extract_html.extact()
+    response = extract_html.extract()
     print()
     print(response)
+
+    assert isinstance(response, ExtractContract)
+
+def test_extract_error():
+    # Importing tested class
+    http_requester = 'This is error'
+    html_extractor = HtmlCollector()
+
+    # Object testing intancing (Dependency Injection)
+    extract_html = ExtractHtml(http_requester,html_extractor)
+
+    try:
+        extract_html.extract()
+    except Exception as exception: # pylint: disable=broad-except
+        assert isinstance(exception, ExtractError)
